3574b0614f147c4ab79bb2f7234a11e4c5bfc665
varshiniI3 <saivarshiniteja@gmail.com>
2024-06-28 22:02:56 +0530

    nav home

--- app/src/components/Banner.js
+++ app/src/components/Banner.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function banner() {
+function Banner() {
   return (
-    <div>
-      <img src="https://mmgihouston.com/wp-content/uploads/2017/09/Services-Header.jpg" className="img-fluid" alt="OUR SERVICES"></img>
+    <div className="container">
+      <img
+        src="https://mmgihouston.com/wp-content/uploads/2017/09/Services-Header.jpg"
+        className="img-fluid"
+        alt="OUR SERVICES"
+      />
     </div>
   );
 }
 
-export default banner;
+export default Banner;
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function Header() {
   return (
+   <>
     <header className="text-center py-5">
       <h1 style={{ color: "#3DB166" }}>Services</h1>
     </header>
+    <button className="button">Home</button>
+    </>
   );
 }
 
